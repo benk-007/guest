16d58565fbbf808152bf1bd34f4480d7a9582f00
Hamza HABCHI <hamza.habchi@messaging-technologies.com>
2025-07-22 20:26:30 +0100

    feat: add dataloader to create segments at startup

---
+++ src/main/java/com/smsmode/guest/service/DataLoaderService.java
@@ -0,0 +1,17 @@
+/**
+ * <p>Copyright (C) Calade Technologies, Inc - All Rights Reserved Unauthorized copying of this
+ * file, via any medium is strictly prohibited Proprietary and confidential
+ */
+package com.smsmode.guest.service;
+
+/**
+ * TODO: add your documentation
+ *
+ * @author hamzahabchi (contact: hamza.habchi@messaging-technologies.com)
+ * <p>Created 22 Jul 2025</p>
+ */
+public interface DataLoaderService {
+
+    void createSegments();
+
+}
